bbe3d72d9006fdb2298caec3e959bd714a1fdd21
Eric Smith <eric@8thlight.com>
2009-07-01 15:56:10 -0500

    Progress on the location part.  Testing JQuery....whew

---
+++ public/javascripts/slide.js
@@ -0,0 +1,4 @@
+$.fn.slidingReplacement = function(new_div, direction) {
+	$(this).hide();
+	$(new_div).show();
+};
---
+++ public/test/javascripts/JsSimpleUnit.js
@@ -0,0 +1,36 @@
+// Copyright © 2009 James Suchy.
+// JsSimpleUnit Copyrights and all included source files are distributed under terms of the GNU LGPL.
+
+function test(tests) {
+  var passed = 0;
+  var failed = 0 ;
+  
+  function write(message, passed) {
+    var output = document.createElement("div");
+    output.innerHTML = message;
+    output.className = "test_result";
+    output.className += passed ? " passed" : " failed";
+    document.body.appendChild(output);
+  }
+  
+  for (testName in tests) {
+    try {
+      tests[testName]();
+      write(testName + " passed", true);
+      passed++;
+    }
+    catch (ex) {
+      write(testName + " failed: " + ex.message, false);
+      failed++;
+    }
+  }
+  
+  write(passed + " tests passed", true);
+  write(failed + " tests failed", false);
+}
+
+test.assertEquals = function(actualValue, expectedValue) {
+  if (actualValue !== expectedValue) {
+    throw new Error("Expected " + expectedValue + " but got " + actualValue);
+  }
+}
