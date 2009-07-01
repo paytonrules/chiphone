88b0d8e1ca9a92559ae7f49a4f7c768ce02cfa7c
Eric Smith <eric@8thlight.com>
2009-07-01 17:25:58 -0500

    Doh - oh well.  Try again later

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -1,4 +1,15 @@
+$.fn.slideLeft = function() {
+	$(this).show("slide", { 
+				direction: "left"
+				});
+}
+
 $.fn.slidingReplacement = function(new_div, direction) {
+	/*
+	$(this).blindLeft();
+	$(new_div).slideLeft();
+	*/
+	// Note this is temporary - I want my kick ass sliding.
 	$(this).hide();
 	$(new_div).show();
 };
