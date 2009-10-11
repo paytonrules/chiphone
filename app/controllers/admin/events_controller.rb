57fefbc736126384783784f982c39c1fbb27f91b
Eric Smith <eric@8thlight.com>
2009-10-11 10:16:18 -0500

    Working events model

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,6 +1,8 @@
 class Admin::EventsController < Admin::ApplicationController
-  active_scaffold :event do |config|
-    config.actions = [:list, :create, :update]
-    config.list.columns = [:event_date, :description]
+  layout "admin"
+  
+  active_scaffold :event do |config|    
+    config.columns = [:event_date, 
+                       :description]
   end
 end
