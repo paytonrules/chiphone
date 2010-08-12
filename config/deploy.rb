b061c4ce87ba9c32468eec4526491aeb9a291f44
Eric Smith <eric@8thlight.com>
2010-08-12 00:26:27 -0500

    New deploy from github.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,8 @@ set :domain, 'chiphonegroup.org'
 set :application, 'chiphone'
 
 # the rest should be good
-set :repository,  "#{user}@#{domain}:git/#{application}.git" 
-set :deploy_to, "/home/#{user}/#{domain}" 
+set :repository,  "git@github.com:paytonrules/#{application}.git"
+set :deploy_to, "/home/#{user}/#{domain}"
 set :deploy_via, :remote_cache
 set :scm, 'git'
 set :branch, 'master'
@@ -20,6 +20,6 @@ role :db, domain, :primary => true
 
 namespace :deploy do
   task :restart do
-    run "touch #{current_path}/tmp/restart.txt" 
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
